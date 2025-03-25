075f9777844be55062613269e7fb3ff78c238bcf
Juan Medina <jmedinaguerrero847@gmail.com>
2025-03-24 20:10:45 -0500

    Add environment configuration and improve application setup
    - Update .gitignore to include all environment files
    - Create env.d.ts for TypeScript environment variable definitions
    - Integrate ConfigModule for environment variable management in AppModule
    - Enhance main.ts with middleware for logging, CORS, and global validation
    - Add DataSource configuration for PostgreSQL using environment variables
    - Update package.json to include necessary dependencies for configuration and logging

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+
+const envFilePath =
+  process.env.NODE_ENV?.trim() === 'production' ? '.env' : '.env.development';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath,
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import * as morgan from 'morgan';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.disable('x-powered-by');
+  app.enableCors();
+  app.use(morgan('dev'));
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(new ValidationPipe());
+
+  const configService = app.get(ConfigService);
+  const PORT = configService.get('PORT') ?? 3000;
+  await app.listen(PORT);
+  console.log(`PORT ${PORT}`);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
